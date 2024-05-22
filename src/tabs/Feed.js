57a91950d1a9d2b569df5fd0c79be43b7ef3de3c
KAIO GONCALVES SANTOS <kaio.gsantos3@senacsp.edu.br>
2024-05-22 15:05:41 -0300

    Mundaça de cor de fundo da Feed

--- src/tabs/Feed.js
+++ src/tabs/Feed.js
@@ -14,7 +14,7 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: 'green',
+        backgroundColor: 'purple',
     },
 
     text:{
